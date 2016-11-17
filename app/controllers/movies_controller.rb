87081e6ca98069ffb2283d01e7b8cf70fbc1ef93
Brian Eng <brian@brianeng.com>
2016-11-17 13:19:37 -0600

    no cors

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,5 @@
 class MoviesController < ApplicationController
+  before_action :set_headers
   before_action :set_movie, only: [:show, :edit, :update, :destroy]
 
   # GET /movies
@@ -66,6 +67,13 @@ class MoviesController < ApplicationController
   end
 
   private
+    def set_headers
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+      headers['Access-Control-Request-Method'] = '*'
+      headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    end
+    
     # Use callbacks to share common setup or constraints between actions.
     def set_movie
       @movie = Movie.find(params[:id])
